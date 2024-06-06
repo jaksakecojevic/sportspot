90beddd2d2ac5dbe3a8810262c44f6b62cc1f59f
jaksakecojevic <kecojevicjaksa@gmail.com>
2024-06-06 10:56:05 +0200

    Footer year set to 2024.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,29 +7,47 @@ export default function Footer() {
     return (
         <footer className="">
             <div className="px-sideSpace py-4 bg-primary">
-                <p className="font-semibold text-white text-xl">SportSpot 2023.</p>
+                <p className="font-semibold text-white text-xl">SportSpot 2024.</p>
                 <div className=" text-white">
                     {session.status == "authenticated" ? (
                         <>
-                            <Link href="/nalog" className="block w-fit mt-3">
+                            <Link
+                                href="/nalog"
+                                className="block w-fit mt-3"
+                            >
                                 Nalog
                             </Link>
-                            <Link href="/nalog/rezervacije" className="block w-fit mt-3">
+                            <Link
+                                href="/nalog/rezervacije"
+                                className="block w-fit mt-3"
+                            >
                                 Rezervacije
                             </Link>
-                            <Link href="/nalog/objekti" className="block w-fit mt-3">
+                            <Link
+                                href="/nalog/objekti"
+                                className="block w-fit mt-3"
+                            >
                                 Objekti
                             </Link>
-                            <Link href="/dodaj-objekat" className="block w-fit mt-3">
+                            <Link
+                                href="/dodaj-objekat"
+                                className="block w-fit mt-3"
+                            >
                                 Objavi svoj objekat
                             </Link>
                         </>
                     ) : (
                         <>
-                            <Link href="/register" className="block w-fit mt-3">
+                            <Link
+                                href="/register"
+                                className="block w-fit mt-3"
+                            >
                                 Registracija
                             </Link>
-                            <Link href="/login" className="block w-fit mt-3">
+                            <Link
+                                href="/login"
+                                className="block w-fit mt-3"
+                            >
                                 Login
                             </Link>
                         </>
